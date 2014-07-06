f8ea5a4747b49f93fa7d254ea1e38c1d408f13c9
Raymond Xu <raymondxumail@gmail.com>
2014-07-06 16:51:46 -0700

    scroll anchor added

--- js/core.js
+++ js/core.js
@@ -1,19 +1,37 @@
 $(document).ready(function(){
 	
-	//Check to see if the window is top if not then display button
+	//Display button after scrolling down
 	$(window).scroll(function(){
-		if ($(this).scrollTop() > 720) {
+		if ($(this).scrollTop() > 716) {
 			$('.scrollToTop').fadeIn();
 		} else {
 			$('.scrollToTop').fadeOut();
 		}
 	});
 	
-	//Click event to scroll to top
+	//Click button to scroll to top
 	$('.scrollToTop').click(function(){
 		$('html, body').animate({scrollTop : 0},400);
 		return false;
 	});
+
+
+
+	//Smooth scroll to anchor
+	$(function() {
+ 	$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
 
 
 });
