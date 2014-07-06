e0a74cb2dda7c1b43781160159c23e71e5e482d3
Raymond Xu <raymondxumail@gmail.com>
2014-07-06 16:53:12 -0700

    js scroll

--- js/core.js
+++ js/core.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 	});
 	
 	//Click button to scroll to top
-	$('.scrollToTop').click(function(){
+	$('.scrollToTop').click(function() {
 		$('html, body').animate({scrollTop : 0},400);
 		return false;
 	});
@@ -19,19 +19,17 @@ $(document).ready(function(){
 
 	//Smooth scroll to anchor
 	$(function() {
- 	$('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
+ 		$('a[href*=#]:not([href=#])').click(function() {
+    		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      			var target = $(this.hash);
+      			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      			if (target.length) {
+        			$('html,body').animate({scrollTop: target.offset().top}, 1000);
+        			return false;
+      			}
+    		}
+  		});
+	});
 
 
 });
