834e24663d982b73cd7823847c394db89e45bd13
Raymond Xu <raymondxumail@gmail.com>
2014-07-05 23:18:30 -0700

    consolidate

--- js/core.js
+++ js/core.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	
 	//Check to see if the window is top if not then display button
 	$(window).scroll(function(){
-		if ($(this).scrollTop() > 220) {
+		if ($(this).scrollTop() > 720) {
 			$('.scrollToTop').fadeIn();
 		} else {
 			$('.scrollToTop').fadeOut();
