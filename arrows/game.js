5aed9ffb093c421678cc96c9d3084c378734ed56
Raymond Xu <raymondxumail@gmail.com>
2014-08-11 14:45:27 -0700

    arrows v1

---
+++ arrows/game.js
@@ -0,0 +1,91 @@
+var arrow;
+var score = 0;
+var timer = 15;
+
+function generateRandomArrow()
+{
+	arrow = Math.floor((Math.random()*4)+1);
+	console.log(arrow);
+	displayArrow(arrow);
+}
+
+function displayArrow(n)
+{
+	var img = document.getElementById("image");
+	var r = Math.floor(Math.random()*2);
+	if(r == 0)
+		img.src="images/arrow" + n + ".png";
+	else
+		img.src="images/redarrow" + n + ".png";
+	return false;
+}
+
+function processKeyPress(n)
+{
+	if(timer > 0)
+	{
+		if(arrow == n)
+		{
+			score++;
+			generateRandomArrow();
+		}
+		else
+		{
+			score--;
+		}
+	}
+	displayScore();
+}
+
+function processUp()
+{
+	processKeyPress(1);
+}
+
+function processRight()
+{
+	processKeyPress(2);
+}
+
+function processDown()
+{
+	processKeyPress(3);
+}
+
+function processLeft()
+{
+	processKeyPress(4);
+}
+
+function displayScore()
+{
+	outputDiv.innerHTML = "Score: " + score;
+}
+
+function displayTime()
+{
+	timerDiv.innerHTML = "Time: " + (timer);
+}
+
+function play()
+{
+	displayScore();
+	displayTime();
+	generateRandomArrow();
+	setInterval(function countdown()
+	{
+		if(timer > 0)
+    		timer -= 1;
+    	displayTime();
+	}, 1000);
+}
+play();
+
+
+$(document).keydown(function(e){
+	var key = e.which;
+	if(key == "37") processLeft();
+	else if(key == "38") processUp();
+	else if(key == "39") processRight();
+	else if(key == "40") processDown();
+})
