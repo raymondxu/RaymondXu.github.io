5a0ac3ae6f3ea6bb215bfb2ebcc4b460c4a650be
Raymond Xu <raymondxumail@gmail.com>
2014-08-18 23:59:11 -0700

    sectiontext renovated

--- js/core.js
+++ js/core.js
@@ -8,18 +8,21 @@ $(".btn-trifold.studentbutton").on("click", function() {
   	$("#studentcontainer").show();
     $("#programmercontainer").hide();
     $("#magiciancontainer").hide();
+    $(".animated.fadeInUp.sectiontext").html("Student");
 });
 
 $(".btn-trifold.programmerbutton").on("click", function() {
     $("#programmercontainer").show();
   	$("#studentcontainer").hide();
     $("#magiciancontainer").hide();
+    $(".animated.fadeInUp.sectiontext").html("Programmer");
 });
 
 $(".btn-trifold.magicianbutton").on("click", function() {
     $("#magiciancontainer").show();
   	$("#studentcontainer").hide();
     $("#programmercontainer").hide();
+    $(".animated.fadeInUp.sectiontext").html("Magician");
 });
 
 
@@ -28,16 +31,16 @@ $(".btn-trifold.magicianbutton").on("click", function() {
 
 
 
-	//Smooth scroll to anchor
-	$(function() {
- 		$('a[href*=#]:not([href=#])').click(function() {
-    		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      			var target = $(this.hash);
-      			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      			if (target.length) {
-        			$('html,body').animate({scrollTop: target.offset().top}, 1000);
-        			return false;
-      			}
-    		}
-  		});
-	});
+//Smooth scroll to anchor
+$(function() {
+ 	$('a[href*=#]:not([href=#])').click(function() {
+  		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    			var target = $(this.hash);
+    			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    			if (target.length) {
+      			$('html,body').animate({scrollTop: target.offset().top}, 1000);
+      			return false;
+    			}
+  		}
+		});
+});
