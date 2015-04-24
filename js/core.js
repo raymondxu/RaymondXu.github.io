def0ab355c4120cac0fa74275b2e1bfd2c931916
Raymond Xu <raymondxumail@gmail.com>
2015-04-23 20:31:02 -0400

    Redesign entire website
    Not mobile friendly or content-filled yet

--- js/core.js
+++ js/core.js
@@ -1,13 +1,28 @@
-//Smooth scroll to anchor
-$(function() {
- 	$('a[href*=#]:not([href=#])').click(function() {
-  		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-    			var target = $(this.hash);
-    			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-    			if (target.length) {
-      			$('html,body').animate({scrollTop: target.offset().top}, 1000);
-      			return false;
-    			}
-  		}
-		});
+$("#about").click(function(){
+	selectTab(0);
 });
+
+$("#projects").click(function(){
+	selectTab(1);
+});
+
+$("#contact").click(function(){
+	selectTab(2);
+});
+
+var selectTab = function(index) {
+	navs = ["#about", "#projects", "#contact"];
+	tabs = ["#aboutdiv", "#projectsdiv", "#contactdiv"];
+	for (var i = 0; i < tabs.length; i++) {
+		if (i == index) {
+			$(tabs[i]).show();
+			$(navs[i]).toggleClass("hover-highlight");
+		}
+		else {
+			$(tabs[i]).hide();
+			if ($(navs[i]).hasClass("hover-highlight")) {
+				$(navs[i]).toggleClass("hover-highlight");
+			}
+		}
+	}
+}
