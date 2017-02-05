a33bf8df4fd58ffec5d718b2c544e3148d21769a
Raymond Xu <raymondxumail@gmail.com>
2017-02-05 14:15:44 -0500

    Refactor card code, revert to single page tabbed layout

---
+++ js/core.js
@@ -0,0 +1,33 @@
+// Open up the selected tab and hide the others
+function selectTab(navId) {
+	$('.behind').hide();
+
+	var targetTab = navId + '-div';
+	var targetNav = document.getElementById(navId);
+	var tabs = document.getElementsByClassName('iter');
+
+	for (var i = 0; i < tabs.length; i++) {
+		if (tabs[i].id === targetTab) {
+			$(tabs[i]).show();
+		}
+		else {
+			$(tabs[i]).hide();
+		}
+	}
+}
+
+
+// When a nav is clicked, highlight it and open up the
+// corresponding tab
+previousNavSelected = null;
+
+$('.nav').each(function() {
+	$(this).on('click', function() {
+		selectTab($(this).attr('id'));
+		if (previousNavSelected !== null) {
+			previousNavSelected.classList.toggle('nav-selected');
+		}
+		this.classList.toggle('nav-selected');
+		previousNavSelected = this;
+	});
+});
